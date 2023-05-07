fd6096f0cd7e7fdcb681c155f5cd8bd7d3f710a0
Dina Gutman <dinagut03@gmail.com>
2023-05-07 18:17:18 -0400

    Add project files.

---
+++ ReactWithVS.Web/ClientApp/src/NameDisplay.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+class NameDisplay extends React.Component {
+
+    render() {
+        return (
+           
+            <>
+                {this.props.people.map(p => <tr className={p.age > 65 ? "table-danger" : ""}>
+                    <td>{p.firstName}</td>
+                    <td>{p.lastName}</td>
+                    <td>{p.age}</td>
+                </tr>)}
+            </>
+        );
+    }
+}
+
+export default NameDisplay;
+
